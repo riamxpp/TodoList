4566e79b5bb635f4bae5d0bca9d1a5aa20398e73
Riam <riamstefeson616@gmail.com>
2024-10-12 23:28:30 -0300

    feat: Criando a view de cadastrar tarefa.

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Todo
 
 class TodoListView(ListView):
   model = Todo
+
+class TodoCreateView(CreateView):
+  model = Todo
+  fields = ["title", "deadLine"] 
+  success_url = reverse_lazy("todo_list")
