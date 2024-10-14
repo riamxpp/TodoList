7f31a3a1f007db3e15cf207c9f8994785486bde9
Riam <riamstefeson616@gmail.com>
2024-10-14 14:06:09 -0300

    refactor: Estilizando o formulário de criação de tarefa com a biblioteca crispy.

--- todos/models.py
+++ todos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(verbose_name="Título", max_length=100, null=False, blank=False)
     created_at = models.DateField(auto_now_add=True, null=False, blank=False)
-    deadLine = models.DateField(null=False, blank=False)
+    deadLine = models.DateField(verbose_name="Fim", null=False, blank=False)
     finished_at = models.DateField(null=True)
