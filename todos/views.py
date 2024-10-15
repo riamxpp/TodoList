15c2cfb514ef64896421185867dd2c1670ff6471
Riam <riamstefeson616@gmail.com>
2024-10-15 17:09:03 -0300

    feat: Criando funcionalidade para finalizar uma tarefa.

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from datetime import date
+
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 
 from .models import Todo
 
@@ -19,3 +22,11 @@ class TodoUpdateView(UpdateView):
 class TodoDeleteView(DeleteView):
   model = Todo
   success_url = reverse_lazy("todo_list")
+
+class TodoCompleteView(View):
+  def get(self, resquest, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.finished_at = date.today()
+    todo.save()
+    
+    return redirect("todo_list")
